6d240bbfbed9e5d403c40890cfd3940eb455384f
Harlan Wilton <harlan@harlanzw.com>
2022-09-13 15:29:51 +1000

    fix(vue): export attrs and noscript

--- packages/vue/src/schema.ts
+++ packages/vue/src/schema.ts
@@ -9,8 +9,12 @@ import type {
   Meta as MetaPlain,
   ScriptEntries as ScriptEntriesPlain,
   Script as ScriptPlain,
+  NoscriptEntries as NoscriptEntriesPlain,
+  Noscript as NoscriptPlain,
   StyleEntries as StyleEntriesPlain,
   Style as StylePlain,
+  BodyAttributes as BodyAttributesPlain,
+  HtmlAttributes as HtmlAttributesPlain
 } from '@zhead/schema'
 
 type MaybeRef<T> = T | Ref<T> | ComputedRef<T>
@@ -26,9 +30,13 @@ export type Link = MaybeRefDeep<LinkPlain>
 export type LinkEntries = MaybeRefDeep<LinkEntriesPlain>
 export type Script = MaybeRefDeep<ScriptPlain>
 export type ScriptEntries = MaybeRefDeep<ScriptEntriesPlain>
+export type Noscript = MaybeRefDeep<NoscriptPlain>
+export type NoscriptEntries = MaybeRefDeep<NoscriptEntriesPlain>
 export type Style = MaybeRefDeep<StylePlain>
 export type StyleEntries = MaybeRefDeep<StyleEntriesPlain>
 export type Meta = MaybeRefDeep<MetaPlain>
 export type MetaEntries = MaybeRefDeep<MetaEntriesPlain>
 export type Base = MaybeRefDeep<BasePlain>
 export type MetaFlat = MaybeRefDeep<Partial<MetaFlatPlain>>
+export type BodyAttributes = MaybeRefDeep<BodyAttributesPlain>
+export type HtmlAttributes = MaybeRefDeep<HtmlAttributesPlain>
