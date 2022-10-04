3e2231a6bfc16c861f500b003db2af720efeb354
Harlan Wilton <harlan@harlanzw.com>
2022-10-05 00:36:45 +1100

    fix(schema): add <link> type mime types

--- packages/schema/src/link.ts
+++ packages/schema/src/link.ts
@@ -150,7 +150,7 @@ export interface Link {
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/link#attr-sizes
    */
-  sizes?: string
+  sizes?: 'any' | '16x16x' | '32x32' | '64x64' | '180x180' | string
   /**
    * The title attribute has special semantics on the <link> element.
    * When used on a <link rel="stylesheet"> it defines a default or an alternate stylesheet.
@@ -168,5 +168,79 @@ export interface Link {
    *
    * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/link#attr-type
    */
-  type?: string
+  type?: 'audio/aac' |
+  'application/x-abiword' |
+  'application/x-freearc' |
+  'image/avif' |
+  'video/x-msvideo' |
+  'application/vnd.amazon.ebook' |
+  'application/octet-stream' |
+  'image/bmp' |
+  'application/x-bzip' |
+  'application/x-bzip2' |
+  'application/x-cdf' |
+  'application/x-csh' |
+  'text/css' |
+  'text/csv' |
+  'application/msword' |
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document' |
+  'application/vnd.ms-fontobject' |
+  'application/epub+zip' |
+  'application/gzip' |
+  'image/gif' |
+  'text/html' |
+  'image/vnd.microsoft.icon' |
+  'text/calendar' |
+  'application/java-archive' |
+  'image/jpeg' |
+  'text/javascript' |
+  'application/json' |
+  'application/ld+json' |
+  'audio/midi' |
+  'audio/x-midi' |
+  'audio/mpeg' |
+  'video/mp4' |
+  'video/mpeg' |
+  'application/vnd.apple.installer+xml' |
+  'application/vnd.oasis.opendocument.presentation' |
+  'application/vnd.oasis.opendocument.spreadsheet' |
+  'application/vnd.oasis.opendocument.text' |
+  'audio/ogg' |
+  'video/ogg' |
+  'application/ogg' |
+  'audio/opus' |
+  'font/otf' |
+  'image/png' |
+  'application/pdf' |
+  'application/x-httpd-php' |
+  'application/vnd.ms-powerpoint' |
+  'application/vnd.openxmlformats-officedocument.presentationml.presentation' |
+  'application/vnd.rar' |
+  'application/rtf' |
+  'application/x-sh' |
+  'image/svg+xml' |
+  'application/x-tar' |
+  'image/tiff' |
+  'video/mp2t' |
+  'font/ttf' |
+  'text/plain' |
+  'application/vnd.visio' |
+  'audio/wav' |
+  'audio/webm' |
+  'video/webm' |
+  'image/webp' |
+  'font/woff' |
+  'font/woff2' |
+  'application/xhtml+xml' |
+  'application/vnd.ms-excel' |
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' |
+  'text/xml' |
+  'application/atom+xml' |
+  'application/xml' |
+  'application/vnd.mozilla.xul+xml' |
+  'application/zip' |
+  'video/3gpp' |
+  'audio/3gpp' |
+  'video/3gpp2' |
+  'audio/3gpp2' | string
 }
