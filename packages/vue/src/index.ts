d69967050c19fbc8cac82d125ad87d990f80fb09
Harlan Wilton <harlan@harlanzw.com>
2022-10-31 16:15:01 +1100

    refactor!: drop vue support, moving to unhead

--- packages/vue/src/index.ts
+++
@@ -1,9 +0,0 @@
-import type { MergeHead } from '@zhead/schema'
-import type { ReactiveHead } from '@zhead/schema-vue'
-
-export * from '@zhead/schema-vue'
-export * from './meta'
-
-export function defineHead<T extends MergeHead>(input: ReactiveHead<T>) {
-  return input
-}
